e77d013650d806b15aa0f6e3f8472d77e81135de
Miles Bright <smilesbright@users.noreply.github.com>
2017-09-23 13:56:05 -0500

    Update updatecommentages.js

--- updatecommentages.js
+++ updatecommentages.js
@@ -6,15 +6,23 @@
 
 const jsonfile = require('jsonfile');
 
+const REMOVAL_AGE = 7;
 
-//
-// Run script once per day to increment comment ages.
-//
+
+// Run script once per day. Increment age and remove old comments
 jsonfile.readFile('./comments.json', function(err, obj) {
-
+    // update comment ages and count how many to remove
+    var numberToSplice = 0;
 	obj.comments.forEach(function(listing) {
 		listing.daysOld ++;
-		jsonfile.writeFile('./comments.json', obj);		
+        if (listing.daysOld > REMOVAL_AGE) {
+            numberToSplice ++;
+        }
+		// jsonfile.writeFile('./comments.json', obj);		
 	});
+    if (numberToSplice > 0) {
+        obj.comments.splice(0, numberToSplice);
+    }
+    jsonfile.writeFile('./comments.json', obj);
 });
 
